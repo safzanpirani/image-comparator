ead6a8d4c0386cc61385768a2f1901dbee430c2f
Safzan Pirani <safzanpirani@gmail.com>
2024-07-24 16:28:07 +0530

    update logic

--- script.js
+++ script.js
@@ -4,45 +4,53 @@ const addImageBtn = document.getElementById('add-image');
 const compareBtn = document.getElementById('compare');
 
 let images = [];
+let imageInputs = [];
 
 addImageBtn.addEventListener('click', () => {
   const newInput = document.createElement('input');
   newInput.type = 'file';
   newInput.accept = 'image/*';
   form.appendChild(newInput);
+  imageInputs.push(newInput);
 });
 
 compareBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const imageInputs = document.querySelectorAll('input[type="file"]');
   images = [];
+
+  imageInputs = document.querySelectorAll('input[type="file"]');
 
   imageInputs.forEach((input) => {
     if (input.files.length > 0) {
-      images.push(input.files[0]);
+      const reader = new FileReader();
+      reader.onload = () => {
+        const img = new Image();
+        img.src = reader.result;
+        images.push(img);
+        if (images.length === imageInputs.length) {
+          displayImages();
+        }
+      };
+      reader.readAsDataURL(input.files[0]);
     }
   });
+});
 
-  if (images.length < 2) {
-    alert('Please select at least two images');
-    return;
-  }
-
+function displayImages() {
+  imageContainer.innerHTML = '';
   const imageWidth = 400; // adjust this value to change the image width
-  const imageHeight = imageWidth / (images[0].width / images[0].height);
 
   const canvas = document.createElement('canvas');
   canvas.width = imageWidth * images.length;
-  canvas.height = imageHeight;
+  canvas.height = imageWidth / (images[0].width / images[0].height);
 
   const ctx = canvas.getContext('2d');
 
   images.forEach((image, index) => {
-    ctx.drawImage(image, index * imageWidth, 0, imageWidth, imageHeight);
+    ctx.drawImage(image, index * imageWidth, 0, imageWidth, canvas.height);
   });
 
   const output = document.createElement('img');
   output.src = canvas.toDataURL();
-  imageContainer.innerHTML = '';
   imageContainer.appendChild(output);
-});
+}
